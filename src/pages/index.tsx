9975b7a471adece499d163f9baa6ae6d56ad9e46
Kevin Jerome <31908183+devkevbot@users.noreply.github.com>
2023-04-16 23:08:33 -0700

    refactor: better handle error and loading states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { toast } from "react-toastify";
 
+const WINS_REQUIRED_IN_SERIES = 4;
+
 export async function getStaticProps() {
   const response = await fetch(
     "https://statsapi.web.nhl.com/api/v1/tournaments/playoffs?expand=round.series,schedule.game.seriesSummary&season=20222023"
@@ -41,8 +43,8 @@ export async function getStaticProps() {
             currentGame: z.object({
               seriesSummary: z.object({
                 gameLabel: z.string(),
-                gameTime: z.string().optional(),
-                necessary: z.boolean().optional(),
+                gameTime: z.string().datetime().optional(),
+                necessary: z.coerce.boolean(),
                 seriesStatus: z.string().optional(),
                 seriesStatusShort: z.string().optional(),
               }),
@@ -51,15 +53,15 @@ export async function getStaticProps() {
               .array(
                 z.object({
                   team: z.object({
-                    name: z.string(),
+                    name: z.enum(NHL_TEAM_NAMES),
                   }),
                   seed: z.object({
                     type: z.string(),
                     isTop: z.boolean(),
                   }),
                   seriesRecord: z.object({
-                    wins: z.number(),
-                    losses: z.number(),
+                    wins: z.number().min(0).max(WINS_REQUIRED_IN_SERIES),
+                    losses: z.number().min(0).max(WINS_REQUIRED_IN_SERIES),
                   }),
                 })
               )
@@ -70,13 +72,16 @@ export async function getStaticProps() {
     ),
   });
 
-  const playoffsData = NhlApiSchema.parse(await response.json());
+  const result = NhlApiSchema.safeParse(await response.json());
+  if (!result.success) {
+    console.error(result.error);
+  }
 
   return {
     props: {
-      playoffsData,
+      playoffsData: result.success ? result.data : null,
     },
-    revalidate: 60 * 60,
+    revalidate: 60 * 15,
   };
 }
 
@@ -96,8 +101,8 @@ function Home(props: StaticProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-sky-500 to-sky-200">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-center  text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        <div className="container flex flex-col items-center justify-center gap-4 px-4 py-16 md:gap-8 xl:gap-12">
+          <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             NHL Playoff Predictions
           </h1>
           <h2 className="text-4xl font-bold tracking-tight text-black sm:text-[4rem]">
@@ -114,11 +119,23 @@ function Home(props: StaticProps) {
               </button>
             )}
             {status === "loading" && (
-              <span className="text-4xl font-bold">Loading...</span>
+              <div className="animate-pulse rounded-md bg-black px-4 py-3 text-white">
+                <span className="text-2xl font-bold md:text-3xl">
+                  Loading...
+                </span>
+              </div>
             )}
-            {status === "authenticated" && sessionData && (
-              <RoundsList data={props.playoffsData} />
-            )}
+            {status === "authenticated" &&
+              sessionData &&
+              props.playoffsData && <RoundsList data={props.playoffsData} />}
+            {status === "authenticated" &&
+              sessionData &&
+              props.playoffsData === null && (
+                <h3 className="text-cl text-center md:text-2xl">
+                  We&apos;re experiencing technical difficulties, please try
+                  again later.
+                </h3>
+              )}
           </div>
         </div>
       </main>
@@ -128,7 +145,7 @@ function Home(props: StaticProps) {
 
 export default Home;
 
-type PlayoffsData = StaticProps["playoffsData"];
+type PlayoffsData = Exclude<StaticProps["playoffsData"], null>;
 
 function RoundsList({ data }: { data: PlayoffsData }) {
   const currentRound = data.defaultRound;
@@ -166,7 +183,7 @@ type PlayoffSeries = PlayoffRound["series"];
 
 function SeriesList({ data }: { data: PlayoffSeries }) {
   return (
-    <div className="grid w-full grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+    <div className="grid w-full grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {data.map((series, index) => {
         return <SeriesItem data={series} key={index} />;
       })}
@@ -174,7 +191,28 @@ function SeriesList({ data }: { data: PlayoffSeries }) {
   );
 }
 
-const teamNameToColor: Record<string, string> = {
+const NHL_TEAM_NAMES = [
+  "Boston Bruins",
+  "Florida Panthers",
+  "Carolina Hurricanes",
+  "New York Islanders",
+  "New Jersey Devils",
+  "New York Rangers",
+  "Toronto Maple Leafs",
+  "Tampa Bay Lightning",
+  "Vegas Golden Knights",
+  "Winnipeg Jets",
+  "Edmonton Oilers",
+  "Los Angeles Kings",
+  "Colorado Avalanche",
+  "Seattle Kraken",
+  "Dallas Stars",
+  "Minnesota Wild",
+] as const;
+
+type NhlTeamName = (typeof NHL_TEAM_NAMES)[number];
+
+const teamNameToColor: Record<NhlTeamName, string> = {
   ["Boston Bruins"]: "bg-amber-400",
   ["Florida Panthers"]: "bg-red-700",
   ["Carolina Hurricanes"]: "bg-red-600",
@@ -248,11 +286,15 @@ function SeriesItem({ data }: { data: PlayoffSeries[number] }) {
     bottomSeedTeamScore: bottomSeed.seriesRecord.wins.toString(),
   });
 
-  const topSeedColor = teamNameToColor[topSeed.team.name] ?? "";
-  const bottomSeedColor = teamNameToColor[bottomSeed.team.name] ?? "";
+  const topSeedColor = teamNameToColor[topSeed.team.name];
+  const bottomSeedColor = teamNameToColor[bottomSeed.team.name];
+
+  if (isLoadingPrediction) {
+    return <SeriesItemSkeletonLoader />;
+  }
 
   return (
-    <div className="flex w-full transform flex-col items-center gap-4 rounded-md bg-gradient-to-tl from-sky-200 to-white p-4 drop-shadow-lg duration-100 ease-in-out md:hover:scale-105">
+    <div className="flex max-w-xs transform flex-col items-center gap-4 rounded-md bg-gradient-to-tl from-sky-200 to-white p-4 drop-shadow-lg duration-100 ease-in-out md:hover:scale-105">
       <div className="flex w-full flex-col gap-2 text-center">
         <div className="text-md flex items-center justify-center gap-2 font-semibold md:text-lg">
           <span className={`${topSeedColor} rounded-full p-1.5`}></span>
@@ -288,37 +330,31 @@ function SeriesItem({ data }: { data: PlayoffSeries[number] }) {
       <h3 className="text-md -mb-2 font-semibold md:text-lg">
         Series Prediction
       </h3>
-      {isLoadingPrediction ? (
-        <div className="w-full animate-pulse cursor-progress rounded-md bg-black px-4 py-1 text-white">
-          Loading prediction...
-        </div>
-      ) : (
-        <select
-          className="w-full cursor-pointer rounded-md bg-black px-4 py-1 text-white disabled:cursor-not-allowed disabled:opacity-50"
-          value={seriesPrediction}
-          onChange={(event) =>
-            onChangePrediction({
-              slug: data.names.seriesSlug,
-              score: event.target.value,
-            })
-          }
-          disabled={seriesProgression !== "not-started"}
-        >
-          <option disabled value="">
-            Choose prediction
-          </option>
-          <optgroup label={`${topSeed.team.name} win`}></optgroup>
-          <option value="4-0">4-0 {topSeed.team.name}</option>
-          <option value="4-1">4-1 {topSeed.team.name}</option>
-          <option value="4-2">4-2 {topSeed.team.name}</option>
-          <option value="4-3">4-3 {topSeed.team.name}</option>
-          <optgroup label={`${bottomSeed.team.name} win`}></optgroup>
-          <option value="0-4">4-0 {bottomSeed.team.name}</option>
-          <option value="1-4">4-1 {bottomSeed.team.name}</option>
-          <option value="2-4">4-2 {bottomSeed.team.name}</option>
-          <option value="3-4">4-3 {bottomSeed.team.name}</option>
-        </select>
-      )}
+      <select
+        className="w-full cursor-pointer rounded-md bg-black px-4 py-1 text-white disabled:cursor-not-allowed disabled:opacity-50"
+        value={seriesPrediction}
+        onChange={(event) =>
+          onChangePrediction({
+            slug: data.names.seriesSlug,
+            score: event.target.value,
+          })
+        }
+        disabled={seriesProgression !== "not-started"}
+      >
+        <option disabled value="">
+          Choose prediction
+        </option>
+        <optgroup label={`${topSeed.team.name} win`}></optgroup>
+        <option value="4-0">4-0 {topSeed.team.name}</option>
+        <option value="4-1">4-1 {topSeed.team.name}</option>
+        <option value="4-2">4-2 {topSeed.team.name}</option>
+        <option value="4-3">4-3 {topSeed.team.name}</option>
+        <optgroup label={`${bottomSeed.team.name} win`}></optgroup>
+        <option value="0-4">4-0 {bottomSeed.team.name}</option>
+        <option value="1-4">4-1 {bottomSeed.team.name}</option>
+        <option value="2-4">4-2 {bottomSeed.team.name}</option>
+        <option value="3-4">4-3 {bottomSeed.team.name}</option>
+      </select>
 
       <div className="md:text-md flex flex-col gap-4 text-sm">
         {predictionOutcome === "series-in-progress" && (
@@ -421,9 +457,8 @@ function getIsSeriesOver(
   topSeedWins: SeriesProgressionInputs["topSeedWins"],
   bottomSeedWins: SeriesProgressionInputs["bottomSeedWins"]
 ) {
-  const winsRequiredToAdvance = 4;
   const isSeriesOver = [topSeedWins, bottomSeedWins].some(
-    (value) => value === winsRequiredToAdvance
+    (value) => value === WINS_REQUIRED_IN_SERIES
   );
   return isSeriesOver;
 }
@@ -456,9 +491,9 @@ function getPredictionOutcome({
 }: {
   seriesProgression: SeriesProgression;
   predictedScore: string;
-  topSeedTeamName: string;
+  topSeedTeamName: NhlTeamName;
   topSeedTeamScore: string;
-  bottomSeedTeamName: string;
+  bottomSeedTeamName: NhlTeamName;
   bottomSeedTeamScore: string;
 }): PredictionOutcome {
   if (predictedScore && seriesProgression === "started") {
@@ -540,4 +575,33 @@ function getPredictionOutcome({
   }
 
   return "incorrect";
+}
+
+function SeriesItemSkeletonLoader() {
+  return (
+    <div className="mx-auto w-64 rounded-md bg-gradient-to-tl from-sky-200 to-white p-4 shadow drop-shadow-lg">
+      <div className="flex animate-pulse space-x-4">
+        <div className="flex-1 space-y-6 py-1">
+          <div className="h-4 rounded bg-sky-500"></div>
+          <div className="grid grid-cols-5">
+            <div className="col-start-1 col-end-3 h-2 rounded bg-sky-500"></div>
+            <div className="col-start-3 col-end-4 h-2 rounded bg-sky-500"></div>
+            <div className="col-span-2 col-start-4 h-2 rounded bg-sky-500"></div>
+          </div>
+
+          <div className="h-4 rounded bg-sky-500"></div>
+          <div className="space-y-3">
+            <div className="grid grid-cols-1">
+              <div className="col-span-1 h-10 rounded bg-sky-500"></div>
+            </div>
+          </div>
+          <div className="grid grid-cols-5 grid-rows-3 gap-4">
+            <div className="col-start-2 col-end-5 row-start-1 h-4 rounded bg-sky-500"></div>
+            <div className="col-span-5 row-start-2 h-5 rounded bg-sky-500"></div>
+            <div className="col-start-2 col-end-5 h-6 rounded bg-sky-500"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
